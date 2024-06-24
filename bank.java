69266e6fd8157e7970f510c8043cdc40db7b4335
Hari-V-133748 <nithyahari502@gmail.com>
2024-06-24 19:53:40 +0530

    Add files via upload

---
+++ bank.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class bank {
+    public static void main(String[]args){
+        LinkedList <String>  need= new LinkedList<>();
+        //getting value in list
+        need.add("Aadhar");
+        need.add("CBIL");
+        //checking values in list
+        if(need.contains("Aadhar")&& need.contains("CBIL")){
+            System.out.println("U'r Eligible");
+        }else{
+            System.out.println("U'r not Eligible");
+        }
+    }
+    
+}
+
+

2a201a1cf561a12464c5c051e7687729f92bbc17
Hari-V-133748 <nithyahari502@gmail.com>
2024-06-25 15:54:19 +0530

    array_to_linkedlist.java

--- array_to_linkedlist.java
+++ array_to_linkedlist.java
@@ -1,23 +1,31 @@
-/*
+/********************
  Question:
+ 
 Add the Array and Linked list. 
- */
+
+ ********************/
 
  import java.util.*;
  public class array_to_linkedlist {
      public static void main(String[] args) {
-         LinkedList<String> subject = new LinkedList<>();
-         subject.add("EEE");
-         subject.add("CSE");
-         subject.add("IT");
-         System.out.println("Subject list: " + subject);
-         ArrayList<String> courses = new ArrayList<>();
-         courses.add("Bio Medical");
-         courses.add("ECE");
-         System.out.println("Courses list: " + courses);
-         subject.addAll(courses);
-         System.out.println("Combined list: " + subject);
+         LinkedList<String> c1 = new LinkedList<>();
+         c1.add("EEE");
+         c1.add("CSE");
+         c1.add("IT");
+         System.out.println("Course 1 list: " + c1);
+         ArrayList<String> c2 = new ArrayList<>();
+         c2.add("Bio Medical");
+         c2.add("ECE");
+         System.out.println("Course 2  list: " + c2);
+         c1.addAll(c2);
+         System.out.println("Combined list: " + c1);
      }
  }
  
- 
+ /*****************************
+ OUTPUT:
+Course 1 list: [EEE, CSE, IT]
+Course 2  list: [Bio Medical, ECE]
+Combined list: [EEE, CSE, IT, Bio Medical, ECE]
+
+  ****************************/

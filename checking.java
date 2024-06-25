1bc13eae252177a00d5d283979e54e7e5d038712
Hari-V-133748 <nithyahari502@gmail.com>
2024-06-25 15:06:04 +0530

    Add files via upload

---
+++ checking.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class checking {
+    public static void main(String[] args) {
+        LinkedList <String> tech=new LinkedList<>();
+        //initializing input in linkedlist
+        tech.add("python");
+        tech.add("JAVA");
+        tech.add("Ruby");
+        tech.add("C++");
+        //checking the value
+        if(tech.contains("JAVA")|| tech.contains("Python")){
+            System.out.println("These technical language are Present");
+        }else{
+            System.out.println("Language not present ");
+        }
+        
+    }
+}
